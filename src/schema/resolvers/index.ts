09eca1378322a1bcadb063e70bc4129639e0e13b
mhdiirsyad <irsyadcahkalipucang@gmail.com>
2025-10-28 22:18:08 +0700

    update resolvers/index.ts

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -1,29 +1,34 @@
 import { DateResolver } from "graphql-scalars"
+import { stadionResolvers } from "./stadionResolver"
+import { fieldResolvers } from "./fieldResolver"
 
 export default {
   Query: {
-    stadions: async(_:any, __:any, {prisma}: any) => {
-      return prisma.stadion.findMany({
-        include: {
-          fields: true,
-          images: true,
-        }
-      })
-    }
+    // stadions: async(_:any, __:any, {prisma}: any) => {
+    //   return prisma.stadion.findMany({
+    //     include: {
+    //       fields: true,
+    //       images: true,
+    //     }
+    //   })
+    // }
+    ...stadionResolvers.Query,
+    ...fieldResolvers.Query,
   },
 
   Mutation: {
-    createStadion: async(_:any, args:any, {prisma}: any) => {
-      const {name, description, mapUrl} = args
-      return prisma.stadion.create({
-        data: {
-          name,
-          description,
-          mapUrl,
-        }
-      })
-    }
-  },
+    // createStadion: async(_:any, args:any, {prisma}: any) => {
+    //   const {name, description, mapUrl} = args
+    //   return prisma.stadion.create({
+    //     data: {
+    //       name,
+    //       description,
+    //       mapUrl,
+    //     }
+    //   })
+      ...stadionResolvers.Mutation,
+      ...fieldResolvers.Mutation,
+    },
 
   DateTime: DateResolver
 }
