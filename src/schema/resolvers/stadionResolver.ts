7467d265029e4396e8eeceb4834f8d93090f9fde
mhdiirsyad <irsyadcahkalipucang@gmail.com>
2025-10-28 22:01:41 +0700

    add: resolver for stadion

--- src/schema/resolvers/stadionResolver.ts
+++ src/schema/resolvers/stadionResolver.ts
@@ -4,12 +4,80 @@ export const stadionResolvers = {
   Query: {
     stadions: async () => {
       return prisma.stadion.findMany({
-        include: {fields: true}         
+        include: {
+          fields: true,
+          facilities: true,
+          images: true,
+          operatingHours: true,
+        }
       })
     },
 
-    stadion: async (args: any) => {
-      return prisma.stadion.findUnique(args.id)
+    stadion: async (_: any, { stadionId }: { stadionId: number }) => {
+      return prisma.stadion.findUnique({
+        where: { id: stadionId },
+        include: {
+          fields: true,
+          facilities: true,
+          images: true,
+          operatingHours: true,
+        }
+      })
+    }
+  },
+
+  Mutation: {
+    createStadion: async (
+      _: any,
+      { name, description, mapUrl, facilities, images } : { name: string, description?: string, mapUrl: string, facilities?: { facilityId: number }[], images?: { imageUrl: string }[] }
+    ) => {
+      return await prisma.stadion.create({
+        data: {
+          name,
+          description,
+          mapUrl,
+          facilities: facilities ? {
+            create: facilities.map((f) => ({
+              facilityId: f.facilityId
+            }))
+          } : undefined,
+          images: images ? {
+            create: images.map((i) => ({
+              imageUrl: i.imageUrl
+            }))
+          } : undefined,
+        },
+        include: {
+          facilities: {include: {Facility: true}},
+          images: true,
+        }
+      })
+    },
+
+    updateStadion: async (
+      _: any,
+      { stadionId, name, description, mapUrl }: { stadionId: number, name: string, description?: string, mapUrl: string }
+    ) => {
+      return await prisma.stadion.update({
+        where: { id: stadionId },
+        data: {
+          name,
+          description,
+          mapUrl,
+        }
+      })
+    },
+
+    deleteStadion: async (
+      _: any,
+      { stadionId }: { stadionId: number }
+    ) => {
+      await prisma.imageStadion.deleteMany({ where: { id: stadionId } });
+      await prisma.field.deleteMany({ where: { id: stadionId } });
+      await prisma.operatingHour.deleteMany({ where: { id: stadionId } });
+      return await prisma.stadion.delete({
+        where: { id: stadionId }
+      })
     }
   }
 }
