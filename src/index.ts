96612cad35ca4b3af038e547a681f2961adeba1b
ZoeMohamed <zoemohamed31@gmail.com>
2025-11-01 02:08:15 +0700

    log tidy

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import "dotenv/config"
 import express from "express"
 import { ApolloServer } from "@apollo/server"
 import { expressMiddleware } from "@as-integrations/express5"
+import bodyParser from "body-parser"
 import typeDefs from "./schema/typeDefs.js"
 import resolvers from "./schema/resolvers/index.js"
 import { buildContext } from "./lib/context.js"
-import bodyParser from "body-parser"
 
 const app = express()
 const server = new ApolloServer({
@@ -26,5 +26,5 @@ app.use(
 const port = process.env.PORT || 4001
 
 app.listen(port, () => {
-  console.log(`🚀 Server ready at http://localhost:${port}/graphql`)
+  console.log(`Server ready at http://localhost:${port}/graphql`)
 })
